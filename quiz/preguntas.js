c3b6a430b8020b9227b0e7c005dd21a63d9bea92
Carlos Estrada <kachiiz@gmail.com>
2021-09-28 15:46:18 -0500

    quiz sept/28

---
+++ quiz/preguntas.js
@@ -0,0 +1,43 @@
+// Explica qué regresa la siguiente línea
+document.getElementById('patito')
+
+// Lista 12 etiquetas de HTML
+
+// Lista 15 atributos de CSS
+
+// Menciona los 3 tipos de selectores de CSS que consideres más comunes
+
+// Como se llama la sala donde regularmente tomamos las sesiones
+
+// Como declaramos un arreglo en JS
+
+// Nombre de la etiqueta que 'une' un archivo HTML con un CSS
+
+// Dado el siguiente objeto... como obtenemos el valor del atributo 'nombre'
+const miPerro = {
+  raza: 'corriente',
+  nombre: 'Solovino',
+  edad: 6,
+  color: 'café'
+}
+
+// Cuál es la diferencia entre Git y Github
+
+// Menciona los comandos que regularmente utilizamos para almacenar nuestros cambios en los repositorios
+
+// Qué pasa si coloco un ';' al final de una sentencia de JS
+
+// Nombre y apellido del coach en turno
+
+// Cuál de las siguientes comparaciones resulta en true y por qué
+8 == '8'
+8 === '8'
+
+// Qué operador se utiliza para concatenar cadenas
+
+// Cuál es el valor asignado a la variable 'shalala'
+const nombre = 'Y ahora todos dicen asi, Bailemos con el tuca, bailemos con el nazo, Bailemos con el tuca tuca tucanazo'
+let shalala = nombre[33]
+
+// Cuánto da como resultado la siguiente suma
+13 + '37'
