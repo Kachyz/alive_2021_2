a9958fc6e554de0da2f3ee370607759253f97c9c
Carlos Estrada <kachiiz@gmail.com>
2021-10-05 16:56:40 -0500

    manejo de datos

---
+++ manejoDatos/index.js
@@ -0,0 +1,84 @@
+let info = {
+    statusCode: 200,
+    data: [
+        {
+          nombre: 'Sofía',
+          ciudad: 'Oaxaca',
+          mascotas: [
+            {
+              nombre: 'El michis',
+              especie: 'Gato',
+              edad: 2,
+            },
+            {
+              nombre: 'Solovino',
+              especie: 'Perro',
+              edad: 4,
+            },
+          ]
+        },
+        {
+          nombre: 'Laura',
+          ciudad: 'Tabasco',
+          mascotas: [
+            {
+              nombre: 'Firulais',
+              especie: 'Perro',
+              edad: 7,
+            },
+            {
+              nombre: 'Killer',
+              especie: 'Perro',
+              edad: 1,
+            },
+            {
+              nombre: 'Scooby',
+              especie: 'Perro',
+              edad: 10
+            }
+          ]
+        },
+        {
+          nombre: 'América',
+          ciudad: 'Guadalajara',
+          mascotas: []
+        },
+        {
+          nombre: 'Ana',
+          ciudad: 'Monterrey',
+          mascotas: [
+            {
+              nombre: 'Chocotorro',
+              especie: 'Perico',
+              edad: 3
+            }
+          ]
+        },
+        {
+          nombre: 'Diana',
+          ciudad: 'Oaxaca',
+          mascotas: [
+            {
+              nombre: 'Bagheera',
+                especie: 'Pantera',
+                edad: 12
+            },
+            {
+              nombre: 'Baloo',
+              especie: 'Oso',
+              edad: 11
+            }
+          ]
+        }
+    ]
+};
+
+//  Quién tiene mas perros?
+//
+//  Lista todas las especies animales
+//
+//  Lista aquellas especies que inicien con la letra P
+//
+//  Quién tiene menos animales
+//
+//  Genera un enunciado similar a "Juan tiene 4 animales, sus nombres son : Rocky, Pepe"
